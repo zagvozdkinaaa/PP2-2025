3ddaf9d674abc2ec1a2c71b99cf26780a5f04f90
Анастасия Загвоздкина <zagvozdkinaaa@MacBook-Pro--Anastasia.local>
2025-01-24 13:24:42 +0500

    upd

---
+++ week1/4.py
@@ -0,0 +1,30 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#legal names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#global variables
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
---
+++ week1/8.py
@@ -0,0 +1,24 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+#Slicing
+b = "Hello, World!"
+print(b[-5:-2])
+
+#Modify Strings
+a = "Hello, World!"
+print(a.upper())
+
+#Concatination
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#String Format
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+#Escape Character 
+txt = "We are the so-called \"Vikings\" from the north."
