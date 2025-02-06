92d6a4a43cfc6f9eb897dcba88c82d41e2e2c035
Anastasiya Zagvozdkina <nastya0204z@gmail.com>
2025-02-06 15:32:53 +0500

    upd

---
+++ week3/functions2/4.py
@@ -0,0 +1,91 @@
+def average_IMDB(movies, user_lst):
+    selected_movies=[movie for movie in movies if movie["name"] in user_lst]
+    if not selected_movies:
+        return "No movies found"
+    total_score= sum(movie["imdb"] for movie in selected_movies)
+    return total_score/len(selected_movies)
+    
+
+
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
+
+user_input=input("Write movies saparated by commas: ")
+user_lst=[name.strip() for name in user_input.split(",")]
+
+print(average_IMDB(movies, user_lst))
